f8fff2a255124ad1e73d29842ab5a09be1dd88db
isha vishwakarma <ishavishwakarma29@gmail.com>
2023-12-25 00:41:21 +0530

    ball class

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
+from ball import Ball
+import time
 
 screen = Screen()
 screen.setup(800, 600)
@@ -9,6 +11,7 @@ screen.tracer(0)
 
 paddle1 = Paddle(350, 0)
 paddle2 = Paddle(-350, 0)
+ball = Ball()
 
 screen.listen()
 screen.onkey(paddle1.go_up, "Up")
@@ -21,5 +24,7 @@ flag=True
 
 while flag:
     screen.update()
+    ball.move()
+    time.sleep(0.1)
 
 screen.exitonclick()
